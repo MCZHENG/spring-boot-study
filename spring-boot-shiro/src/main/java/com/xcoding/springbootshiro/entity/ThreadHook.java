d43751014defe1ebeea98abffe19b4b53401df88
zhengmingcong <mc_zheng138@163.com>
2021-03-22 18:52:35 +0800

    自定义锁

---
+++ spring-boot-shiro/src/main/java/com/xcoding/springbootshiro/entity/ThreadHook.java
@@ -0,0 +1,35 @@
+package com.xcoding.springbootshiro.entity;
+
+import java.sql.SQLOutput;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadHook {
+    public static void main(String[] args) {
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override
+            public void run() {
+                try{
+                    System.out.println("threadhook 1 is running.");
+                    TimeUnit.SECONDS.sleep(1);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                System.out.println("threadhook 1 is exit ex");
+            }
+        });
+        //可定义多个钩子线程
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override
+            public void run() {
+                try{
+                    System.out.println("threadhook 2 is running.");
+                    TimeUnit.SECONDS.sleep(1);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                System.out.println("threadhook 2 is exit");
+            }
+        });
+        System.out.println("threadhook is exit stoping..");
+    }
+}
